8af8d4a2f391d62c41ad3f02b44d63a0e2144c22
Matt Kragen <mattdkragen@gmail.com>
2021-05-27 20:01:08 -0700

    Create instance tests for Ride; create Ride class

--- lib/ride.rb
+++ lib/ride.rb
@@ -1,2 +1,13 @@
 class Ride
+  attr_reader :name,
+              :cost,
+              :intensity,
+              :passengers
+  
+  def initialize(name, cost, intensity)
+    @name = name
+    @cost = cost
+    @intensity = intensity
+    @passengers = []
+  end
 end
--- spec/ride_spec.rb
+++ spec/ride_spec.rb
@@ -4,11 +4,21 @@ require './lib/ride'
 RSpec.describe Ride do
   describe 'instantiation' do
     before :each do
-      millennium = Ride.new("Millennium Force", 5, 8)
+      @millennium = Ride.new("Millennium Force", 5, 8)
     end
 
     it 'exists' do
-      expect(millennium).to be_a(Ride)
+      expect(@millennium).to be_a(Ride)
+    end
+
+    it 'has attributes' do
+      expect(@millennium.name).to eq("Millennium Force")
+      expect(@millennium.cost).to eq(5)
+      expect(@millennium.intensity).to eq(8)
+    end
+
+    it 'starts with no passengers' do
+      expect(@millennium.passengers).to eq([])
     end
   end
 end
